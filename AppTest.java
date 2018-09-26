b14c43e47e9ea17bfcc31ce4b19c36e727af902c
SakthiMaheswariK <43263252+SakthiMaheswariK@users.noreply.github.com>
2018-09-26 19:11:56 +0530

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,38 @@
+package com.database.newtable.sql;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class AppTest 
+    extends TestCase
+{
+	
+    private static final String Long = null;
+
+
+	public AppTest( String testName )
+    {
+        super( testName );
+       Long setS_no(int s_no) {
+    		int S_no = s_no;
+    	}
+    	Long getEmp_code() {
+    		Object emp_code;
+			return;
+    	}
+    }
+
+    
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+  
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
